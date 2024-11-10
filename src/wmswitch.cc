e7511806257cdee92585a21a4a794e19e33478dd
Bert Gijsbers <gijsbers@science.uva.nl>
2024-11-10 19:15:28 +0100

    Remove the default 1px black border on the right and bottom sides of the QuickSwitch window when Look is flat for issue ice-wm/icewm#174.

--- src/wmswitch.cc
+++ src/wmswitch.cc
@@ -592,7 +592,7 @@ void SwitchWindow::repaint() {
 void SwitchWindow::paint(Graphics &g, const YRect &/*r*/) {
     int b1 = (wmLook != lookFlat);
     int b2 = b1 * 2;
-    int b3 = b2 + 1;
+    int b3 = b1 * 3;
 
     if (switchbackPixbuf != null &&
         (fGradient == null ||
